ace9f6cdc6ea926165a5068baa6f107963bf4fa5
Justin Charney <justin.charney@gmail.com>
2025-03-16 20:21:27 -0400

    Remove local only check for registering Telescope

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,18 +10,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        if (
-            $this->app->environment("local") &&
-            class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)
-        ) {
-            $this->app->register(
-                \Laravel\Telescope\TelescopeServiceProvider::class
-            );
-            $this->app->register(TelescopeServiceProvider::class);
-        }
-    }
+    public function register(): void {}
 
     /**
      * Bootstrap any application services.
