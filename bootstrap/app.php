589555f73e2da603e5910c23b34161841837cc27
Justin Charney <justin.charney@gmail.com>
2025-03-16 20:00:45 -0400

    Add trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             "verified" => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
+        $middleware->trustProxies(at: "*");
 
         //
     })
