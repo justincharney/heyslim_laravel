87aec902ccd0c31d7f27f720ea6325dc2744f4c4
Justin Charney <justin.charney@gmail.com>
2025-04-08 17:50:41 -0400

    Updating middleware

--- routes/auth.php
+++ routes/auth.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\WorkOSAuthController;
 
-Route::get("login", [WorkOSAuthController::class, "showLogin"])
-    ->middleware(["guest"])
-    ->name("login");
+Route::get("login", [WorkOSAuthController::class, "showLogin"])->name("login");
 
 Route::get("authenticate", [
     WorkOSAuthController::class,
