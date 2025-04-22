534b4fb89dbcdf371f6e241ab2ec848213572b0f
Justin Charney <justin.charney@gmail.com>
2025-04-21 21:03:01 -0400

    Add sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\HorizonBasicAuthMiddleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -33,6 +34,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies(at: "*");
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        Integration::handles($exceptions);
     })
     ->create();
